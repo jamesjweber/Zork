42a0a77cbf7dda9ac55b0c1c214de03e71ffd7bc
Evan Miller <evan.miller@hpe.com>
2017-11-04 18:43:53 -0500

    Skeleton Container Class

---
+++ Zork/Container.cpp
@@ -0,0 +1,27 @@
+/*
+ * Container.cpp
+ *
+ *  Created on: Nov 4, 2017
+ *      Author: millerev
+ */
+
+#include "Container.h"
+
+Container::Container() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Container::~Container() {
+	// TODO Auto-generated destructor stub
+}
+
+bool Container::hasItem(string item){
+	return false;
+}
+void Container::checkTriggers(){
+
+}
+void Container::checkAccept(){
+
+}
---
+++ Zork/Container.h
@@ -0,0 +1,36 @@
+/*
+ * Container.h
+ *
+ *  Created on: Nov 4, 2017
+ *      Author: millerev
+ */
+
+#ifndef ZORK_CONTAINER_H_
+#define ZORK_CONTAINER_H_
+
+#include <string>
+#include <vector>
+#include "Item.h"
+
+class Accept;
+class Trigger;
+
+using namespace std;
+
+class Container {
+public:
+	Container();
+	virtual ~Container();
+	string name;
+	string status;
+	string description;
+	vector<Accept> accepts;
+	vector<Item> items;
+    vector<Trigger> triggers;
+
+    bool hasItem(string item);
+    void checkTriggers();
+    void checkAccept();
+};
+
+#endif /* ZORK_CONTAINER_H_ */
