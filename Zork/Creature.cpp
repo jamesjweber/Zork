020fe0079582f393337c98d55ad1990c7b7fa889
Evan Miller <evan.miller@hpe.com>
2017-11-05 13:32:38 -0600

    creature skeleton files

---
+++ Zork/Creature.cpp
@@ -0,0 +1,26 @@
+/*
+ * Creature.cpp
+ *
+ *  Created on: Nov 5, 2017
+ *      Author: millerev
+ */
+
+#include "Creature.h"
+
+Creature::Creature() {
+	// TODO Auto-generated constructor stub
+
+}
+Creature::Creature(xml_node<>* creatureNode){
+
+}
+
+Creature::~Creature() {
+	// TODO Auto-generated destructor stub
+}
+void Creature::attack(){
+
+}
+void Creature::checkTriggers(){
+
+}
